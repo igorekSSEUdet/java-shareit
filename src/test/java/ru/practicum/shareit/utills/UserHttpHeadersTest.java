30af08f5899cbdff78cff69e71ab437829bcbad2
igorekSSEUdet <korablev_sseu@mail.ru>
2023-06-14 13:11:05 +0400

    fix checkstyle

--- src/test/java/ru/practicum/shareit/utills/UserHttpHeadersTest.java
+++ src/test/java/ru/practicum/shareit/utills/UserHttpHeadersTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserHttpHeadersTest {
 
     @Test
-    public void UserHttpHeadersTest() {
+    public void userHttpHeadersTest() {
         UserHttpHeaders userHttpHeaders = new UserHttpHeaders();
         assertEquals(userHttpHeaders.hashCode(), 0);
         assertEquals(UserHttpHeaders.USER_ID, "X-Sharer-User-Id");
